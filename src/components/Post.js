868f0ecc2fc1f54703b68b342a53a9f52adb2512
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-06-30 21:58:57 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,9 +2,10 @@ import React from 'react'
 const Post = () => {
     return (
         <div className="post">
-
+           <h3>{ele.title}</h3>
+            <p>{ele.body}</p>
         </div>
-    )
-}
+    );
+};
 
-export { Post }
+export { Post };
